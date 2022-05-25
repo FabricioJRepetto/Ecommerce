84c5e50e03e2cc02fb019584b8ff8c8e7c02da1f
Fabricio Repetto <93217713+FabricioJRepetto@users.noreply.github.com>
2022-05-25 01:28:11 -0300

    crud v1.0

--- back/src/routes/products.router.js
+++ back/src/routes/products.router.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const router = Router();
-const { getAll, getById, createProduct, updateProduct, deleteProduct} = require('../controllers/products.ctrl.js');
+const { getAll, getById, createProduct, updateProduct, deleteProduct, deleteAll} = require('../controllers/products.ctrl.js');
 
 router.get('/', getAll);
 router.get('/:id', getById);
 router.post('/', createProduct);
-router.put('/', updateProduct);
-router.delete('/', deleteProduct);
+router.put('/:id', updateProduct);
+router.delete('/deleteall', deleteAll);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
