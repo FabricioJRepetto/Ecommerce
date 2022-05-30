cd1520ae981e717918d0c8d557846066aaefdcd5
Fer Ramirez <fer.eze.ram@gmail.com>
2022-05-30 20:30:05 -0300

    roles added

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -14,6 +14,11 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   }, */
+  role: {
+    type: String,
+    enum: ["client", "admin"],
+    default: "client",
+  },
   password: {
     type: String,
     required: true,
