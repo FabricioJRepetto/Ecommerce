dc7d75bf295d7e1de06776548fe5c1e167455464
Fer Ramirez <fer.eze.ram@gmail.com>
2022-05-31 22:37:57 -0300

    google verify token added

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 require("dotenv").config();
-const { JWT_SECRET_CODE } = process.env;
+const { JWT_SECRET_CODE, OAUTH_CLIENT_ID } = process.env;
+const { OAuth2Client } = require("google-auth-library");
 
 module.exports = async (req, res, next) => {
   try {
@@ -9,11 +10,31 @@ module.exports = async (req, res, next) => {
     if (!authHeader) res.status(403).json({ message: "No token provided" });
 
     const token = authHeader.split(" ")[1];
-    const userDecoded = jwt.verify(token, JWT_SECRET_CODE);
-    req.user = userDecoded.user;
+    const isGoogleUser = token.slice(0, 6);
 
-    const userFound = await User.findById(req.user._id);
-    if (!userFound) return res.status(404).json({ message: "No user found" });
+    if (isGoogleUser === "google") {
+      const googleToken = token.substring(6);
+
+      const client = new OAuth2Client(OAUTH_CLIENT_ID);
+      try {
+        const ticket = await client.verifyIdToken({
+          idToken: googleToken,
+          audience: OAUTH_CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        req.user = { _id: payload["sub"] };
+      } catch (error) {
+        return res.status(400).send("Invalid credentials");
+      }
+    } else {
+      const userDecoded = jwt.verify(token, JWT_SECRET_CODE);
+      req.user = userDecoded.user;
+
+      const userFound = await User.findById(req.user._id);
+      if (!userFound)
+        return res.status(404).json({ message: "User not found" });
+    }
+
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
