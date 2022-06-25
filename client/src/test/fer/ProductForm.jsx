19a6a04b77fdfc1117a9f327e41b89a23db688da
Fer Ramirez <fer.eze.ram@gmail.com>
2022-06-24 21:22:05 -0300

    img remove working

--- client/src/test/fer/ProductForm.jsx
+++ client/src/test/fer/ProductForm.jsx
@@ -4,6 +4,7 @@ import { useForm, useFieldArray } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useEffect } from "react";
+import "./ProductForm.css";
 
 const validatePrice = (value) => {
   if (value === undefined) return false;
@@ -16,29 +17,28 @@ const validateNumbers = (value) => {
 };
 
 const clearInputs = (
-  /* featuresQuantity,
+  featuresQuantity,
   setFeaturesQuantity,
   removeFeature,
-  appendFeature, */
+  appendFeature,
   attributesQuantity,
   setAttributesQuantity,
   removeAttribute,
   appendAttribute
 ) => {
-  /* const ids = ["name_id", "price_id", "brand_id", "stock_id", "description_id"];
+  const ids = ["name_id", "price_id", "brand_id", "stock_id", "description_id"];
   for (const id of ids) {
     let input = document.getElementById(id);
     input.value = "";
-  } */
-  /* if (featuresQuantity > 1) {
+  }
+  if (featuresQuantity > 1) {
     removeFeature();
     setFeaturesQuantity(1);
     appendFeature("");
   } else {
     let mainFeature = document.getElementById("main_feature_0");
     mainFeature.value = "";
-  } */
-  console.log(attributesQuantity);
+  }
   if (attributesQuantity > 1) {
     removeAttribute();
     setAttributesQuantity(1);
@@ -52,9 +52,10 @@ const clearInputs = (
 };
 
 const ProductForm = () => {
-  // const [productImg, setProductImg] = useState();
+  const [productImg, setProductImg] = useState([]);
+  const [productImgUrls, setProductImgUrls] = useState([]);
   // const [featuresQuantity, setFeaturesQuantity] = useState(1);
-  const [attributesQuantity, setAttributesQuantity] = useState(1);
+  // const [attributesQuantity, setAttributesQuantity] = useState(1);
 
   const validationSchema = yup.object().shape({
     /* name: yup.string().required("Nombre es requerido"),
@@ -74,13 +75,13 @@ const ProductForm = () => {
     description: yup.string().required("Descripción es requerida"), 
     main_features: yup
       .array()
-      .of(yup.string().required("Principales características requeridas")),*/
+      .of(yup.string().required("Principales características requeridas")),
     attributes: yup.array().of(
       yup.object().shape({
         name: yup.string().required("Nombre de atributo es requerido"),
         value_name: yup.string().required("Valor de atributo es requerido"),
       })
-    ),
+    ),*/
   });
 
   const formOptions = { resolver: yupResolver(validationSchema) };
@@ -123,7 +124,7 @@ const ProductForm = () => {
     appendFeature("");
   }, []); */
 
-  const {
+  /*   const {
     fields: fieldsAttributes,
     append: appendAttribute,
     remove: removeAttribute,
@@ -158,42 +159,57 @@ const ProductForm = () => {
   useEffect(() => {
     appendAttribute({ name: "", value_name: "" });
     // eslint-disable-next-line
-  }, []);
+  }, []); */
 
   const submitProduct = async (productData) => {
+    if (productImg.length === 0) return console.log("subir img"); //!VOLVER A VER renderizar mensaje warn
     console.log(productData);
-    // let formData = new FormData();
+    let formData = new FormData();
     //: verificar datos
 
     // agarra las images
-    /*   productImg.forEach((pic) => {
-        formData.append("images", pic);
-      }); 
-    formData.append('images', productImg);
-    formData.append("data", JSON.stringify(productData)); */
+    productImg.forEach((pic) => {
+      formData.append("images", pic);
+    });
+    formData.append("images", productImg);
+    formData.append("data", JSON.stringify(productData));
 
-    /*   const imgURL = await axios.post(`/product/`, formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      console.log(imgURL); */
+    const imgURL = await axios.post(`/product/`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log(imgURL);
 
-    clearInputs(
-      /* featuresQuantity,
+    /* clearInputs(
+       featuresQuantity,
       setFeaturesQuantity,
       removeFeature,
-      appendFeature, */
+      appendFeature, 
       attributesQuantity,
       setAttributesQuantity,
       removeAttribute,
       appendAttribute
-    );
+    ); */
   };
 
-  /*   const handleFiles = (e) => {
-    setProductImg([...e.target.files]);
-  }; */
+  useEffect(() => {
+    //  if (productImg.length > 0) {
+    const newImageUrls = [];
+    for (const image of productImg) {
+      newImageUrls.push(URL.createObjectURL(image));
+    }
+    setProductImgUrls(newImageUrls);
+    // }
+  }, [productImg]);
+
+  const handleFiles = (e) => {
+    setProductImg([...productImg, ...e.target.files]);
+  };
+
+  const handleRemoveImg = (i) => {
+    setProductImg(productImg.filter((_, index) => index !== i));
+  };
 
   return (
     <div>
@@ -204,7 +220,7 @@ const ProductForm = () => {
         onSubmit={handleSubmit(submitProduct)}
       >
         <>
-          {/* <div>
+          {/*  <div>
           <input
             type="text"
             placeholder="Título/Nombre"
@@ -246,11 +262,11 @@ const ProductForm = () => {
         </div>
         <br />
         <hr />
-        <br /> */}
-        </>
+        <br />
+        
         <div>
-          <>
-            {/* {React.Children.toArray(
+
+            {React.Children.toArray(
             fieldsFeatures.map((_, i) => (
               <>
                 <input
@@ -271,8 +287,8 @@ const ProductForm = () => {
 
           <br />
           <hr />
-          <br /> */}
-          </>
+          <br />
+
 
           {React.Children.toArray(
             fieldsAttributes.map((_, i) => (
@@ -280,7 +296,6 @@ const ProductForm = () => {
                 <input
                   type="text"
                   placeholder="Nombre del atributo"
-                  /*  name={`attributes[${i}]name`} //!VOLVER A VER COMENTAR ESTO */
                   autoComplete="off"
                   id={`attribute_name_${i}`}
                   {...register(`attributes.${i}.name`)}
@@ -289,7 +304,6 @@ const ProductForm = () => {
                 <input
                   type="text"
                   placeholder="Valor del atributo"
-                  /* name={`attributes[${i}]value_name`} */
                   autoComplete="off"
                   id={`attribute_value_${i}`}
                   {...register(`attributes.${i}.value_name`)}
@@ -302,9 +316,8 @@ const ProductForm = () => {
           <h3 onClick={() => handleAddAttribute()}>
             Agregar campos de atributo
           </h3>
-        </div>
-        <>
-          {/*  <br />
+        </div> 
+            <br />
         <hr />
         <br />
         <div>
@@ -321,24 +334,41 @@ const ProductForm = () => {
         </>
 
         <div>
-          {/*           <input
+          <label htmlFor="filesButton">BOTON PARA SUBIR IMAGENES</label>
+          <input
             type="file"
             name="image"
             multiple
             accept=".jpeg,.jpg,.png"
             onChange={handleFiles}
-          /> */}
+            style={{ visibility: "hidden" }}
+            id="filesButton"
+          />
         </div>
+
+        {React.Children.toArray(
+          productImgUrls.map((imageUrl, i) => (
+            <>
+              <img
+                src={imageUrl}
+                alt="sd"
+                className="imgs-product"
+                /*  id={`product_img_${i}`} */
+              />
+              <span onClick={() => handleRemoveImg(i)}> X</span>
+            </>
+          ))
+        )}
 
         <input type="submit" value="Crear producto" />
       </form>
-      <button
+      {/*  <button
         onClick={() =>
           clearInputs(
-            /* featuresQuantity,
+             featuresQuantity,
       setFeaturesQuantity,
       removeFeature,
-      appendFeature, */
+      appendFeature, 
             attributesQuantity,
             setAttributesQuantity,
             removeAttribute,
@@ -347,7 +377,7 @@ const ProductForm = () => {
         }
       >
         RESETEAR
-      </button>
+      </button> */}
     </div>
   );
 };
