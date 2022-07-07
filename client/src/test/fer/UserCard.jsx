95927f81c07e482a33cae31de5bb8a2bb5884d2d
Fer Ramirez <fer.eze.ram@gmail.com>
2022-07-07 17:12:37 -0300

    delete modal moved

--- client/src/test/fer/UserCard.jsx
+++ client/src/test/fer/UserCard.jsx
@@ -1,27 +1,19 @@
-import axios from "axios";
-
-const UserCard = ({ user, setAllUsersData, allUsersData }) => {
+const UserCard = ({ user, openDeleteUser }) => {
   const { name, email, role, emailVerified, avatar, _id } = user;
 
-  const deleteUser = () => {
-    //! VOLVER A VER agregar confirmacion
-    axios
-      .delete(`/user/${_id}`)
-      .then((res) => {
-        setAllUsersData(allUsersData.filter((user) => user._id !== _id));
-        console.log("eliminadp");
-      }) //! VOLVER A VER agregar notif
-      .catch((err) => console.log(err));
-  };
+  const handleRole = () => {};
 
   return (
     <div>
-      <h2>{name}</h2>
-      <h4>Email: {email}</h4>
-      <h4>Rol: {role}</h4>
-      <h4>Verificado: {emailVerified ? "si" : "no"}</h4>
-      <h4>{avatar}</h4>
-      <button onClick={deleteUser}>Eliminar</button>
+      <div>
+        <h2>{name}</h2>
+        <h4>Email: {email}</h4>
+        <h4>Rol: {role}</h4>
+        <h4>Verificado: {emailVerified ? "si" : "no"}</h4>
+        <h4>{avatar}</h4>
+        <button onClick={() => openDeleteUser({ _id, name })}>Eliminar</button>
+        <button onClick={handleRole}>Cambiar Rol</button>
+      </div>
       <hr />
     </div>
   );
--- client/src/test/fer/Users.jsx
+++ client/src/test/fer/Users.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import UserCard from "./UserCard";
+import { useModal } from "../../hooks/useModal";
+import Modal from "../../components/common/Modal";
+import { useNotification } from "../../hooks/useNotification";
 
 const Users = () => {
   const [usersData, setUsersData] = useState([]);
+  const [isOpenDeleteUser, openDeleteUser, closeDeleteUser, userToDelete] =
+    useModal();
+  const [notification] = useNotification();
 
   useEffect(() => {
     axios("/user/getAll")
@@ -11,17 +17,46 @@ const Users = () => {
       .catch((err) => console.log(err)); //! VOLVER A VER manejo de errores
   }, []);
 
+  const handleDeleteUser = () => {
+    deleteUser();
+    closeDeleteUser();
+  };
+
+  const deleteUser = () => {
+    axios
+      .delete(`/user/${userToDelete._id}`)
+      .then((_) => {
+        setUsersData(usersData.filter((user) => user._id !== userToDelete._id));
+        notification("Usuario eliminado exitosamente", "", "success");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
-      {React.Children.toArray(
-        usersData?.map((user) => (
-          <UserCard
-            user={user}
-            setAllUsersData={setUsersData}
-            allUsersData={usersData}
-          />
-        ))
-      )}
+      <div>
+        {React.Children.toArray(
+          usersData?.map((user) => (
+            <UserCard
+              user={user}
+              setAllUsersData={setUsersData}
+              allUsersData={usersData}
+              openDeleteUser={openDeleteUser}
+            />
+          ))
+        )}
+      </div>
+      <Modal isOpen={isOpenDeleteUser} closeModal={closeDeleteUser} type="warn">
+        <p>{`¿Eliminar al usuario ${
+          userToDelete ? userToDelete.name : null
+        }?`}</p>
+        <button type="button" onClick={() => handleDeleteUser()}>
+          Aceptar
+        </button>
+        <button type="button" onClick={closeDeleteUser}>
+          Cancelar
+        </button>
+      </Modal>
     </div>
   );
 };
