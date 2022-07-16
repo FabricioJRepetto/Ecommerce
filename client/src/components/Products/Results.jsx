cd84058812083fa075d8af1a240eef5c9f59eed2
Fabricio Repetto <93217713+FabricioJRepetto@users.noreply.github.com>
2022-07-15 21:04:19 -0300

    filters style

--- client/src/components/Products/Results.jsx
+++ client/src/components/Products/Results.jsx
@@ -11,8 +11,10 @@ import {
  } from '../../Redux/reducer/productsSlice'
 import Card from './Card'
 import {ReactComponent as Spinner } from '../../assets/svg/spinner.svg'
+import {ReactComponent as Arrow } from '../../assets/svg/arrow-right.svg'
 
 import './Results.css'
+import { useState } from 'react'
 
 const Results = () => {
     const whishlist = useSelector((state) => state.cartReducer.whishlist);
@@ -22,6 +24,8 @@ const Results = () => {
     const applied = useSelector((state) => state.productsReducer.productsAppliedFilters);
     const productsFilters = useSelector((state) => state.productsReducer.productsFilters);
     const breadCrumbs = useSelector((state) => state.productsReducer.breadCrumbs);
+
+    const [open, setOpen] = useState('')
 
     const dispatch = useDispatch();
 
@@ -42,8 +46,6 @@ const Results = () => {
         // eslint-disable-next-line
     }, [querys]);
 
-    console.log(productsOwn);
-
     const addFilter = async (obj) => {
         let filter = obj.filter;
         let value = obj.value;
@@ -58,88 +60,90 @@ const Results = () => {
 
   return (
     <div className='results-container'>
-        <div className="results-filters">
-            <h2>Filtros</h2>
-            <div>
-                <b>filtros aplicados</b>
-                {applied.length > 0 && 
-                    React.Children.toArray(applied.map(f => (
-                        <div onClick={()=>removeFilter(f.id)}>{f.values[0].name}</div>
-                    )))
-                }
-            </div>
-            <hr />
-            {(productsFilters !== 'loading' && productsFilters.length > 0) &&
-                    React.Children.toArray(productsFilters?.map(f => (
-                        <div key={f.id}>
-                            <b>{f.name}</b>
-                            {f.values.map(v => (
-                                <div key={v.id} onClick={() => addFilter({filter: f.id, value: v.id })}>
-                                    <p>{`${v.name} (${v.results})`}</p>
-                                </div>
-                            ))}
-                        </div>
-                    )))
+        {/* BREAD CRUMBS */}
+        <div className='bread-crumbs'>
+            {breadCrumbs?.length > 0 &&
+                React.Children.toArray(
+                    breadCrumbs.map((c, index) => (
+                        <span key={c.id} onClick={ () => addFilter({filter: 'category', value: c.id})}>{ (index > 0 ? ' > ' : '') + c.name }</span>
+                    ))
+                )
             }
         </div>
 
-        <div className='results-inner'>
-            <h2>Results</h2>
-            {/* BREAD CRUMBS */}
-            <div>
-                {breadCrumbs?.length > 0 &&
-                    React.Children.toArray(
-                        breadCrumbs.map(c => (
-                            <span key={c.id} onClick={ () => addFilter({filter: 'category', value: c.id})}>{c.name + ' > '}</span>
-                        ))
-                    )
+        <div className='results-container-inner'>
+
+            <div className="results-filters">
+                <div>
+                    {applied.length > 0 && 
+                        React.Children.toArray(applied.map(f => (
+                            <div onClick={()=>removeFilter(f.id)}>{f.values[0].name}</div>
+                        )))
+                    }
+                </div>
+                {(productsFilters !== 'loading' && productsFilters.length > 0) &&
+                        React.Children.toArray(productsFilters?.map(f => (
+                            <div key={f.id} className={`results-filter-container ${open === f.id && 'open-filter'}`}>
+                                <div onClick={() => setOpen(open === f.id ? '' : f.id)} className='filter-title'>
+                                    <b>{f.name}</b>
+                                    <Arrow className={`filters-arrow-svg ${open === f.id && 'open-arrow'}`}/>
+                                </div>
+                                {f.values.map(v => (
+                                    <div key={v.id} onClick={() => addFilter({filter: f.id, value: v.id })} className='results-filter-option'>
+                                        <p>{`${v.name} (${v.results})`}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        )))
                 }
             </div>
 
-            {(productsFound !== 'loading' && (productsFound.length > 0 || productsOwn.length > 0))
-            ? <div>
-                <div className='own-products-container'>
-                    {productsOwn.length > 0 && React.Children.toArray(
-                    productsOwn?.map(prod => (
-                        <Card
-                            img={prod.thumbnail}
-                            name={prod.name}
-                            price={prod.price}
-                            sale_price={prod.sale_price}
-                            discount={prod.discount}
-                            brand={prod.brand}
-                            prodId={prod._id}
-                            free_shipping={prod.free_shipping}
-                            fav={whishlist.includes(prod._id)}
-                            on_sale={prod.on_sale}
-                        />
-                    ))
-                )}
+            <div className='results-inner'>
+                {(productsFound !== 'loading' && (productsFound.length > 0 || productsOwn.length > 0))
+                ? <div>
+                    {productsOwn.length > 0 && <div className='own-products-container'>
+                        {React.Children.toArray(
+                            productsOwn?.map(prod => (
+                                <Card
+                                    img={prod.thumbnail}
+                                    name={prod.name}
+                                    price={prod.price}
+                                    sale_price={prod.sale_price}
+                                    discount={prod.discount}
+                                    brand={prod.brand}
+                                    prodId={prod._id}
+                                    free_shipping={prod.free_shipping}
+                                    fav={whishlist.includes(prod._id)}
+                                    on_sale={prod.on_sale}
+                                />
+                            ))
+                        )}
+                    </div>}
+                    {React.Children.toArray(
+                        productsFound?.map(prod => (
+                            <Card
+                                img={prod.thumbnail}
+                                name={prod.name}
+                                price={prod.price}
+                                sale_price={prod.sale_price}
+                                discount={prod.discount}
+                                brand={prod.brand}
+                                prodId={prod._id}
+                                free_shipping={prod.free_shipping}
+                                fav={whishlist.includes(prod._id)}
+                                on_sale={prod.on_sale}
+                            />
+                        ))
+                    )}
                 </div>
-                <br />
-                {React.Children.toArray(
-                    productsFound?.map(prod => (
-                        <Card
-                            img={prod.thumbnail}
-                            name={prod.name}
-                            price={prod.price}
-                            sale_price={prod.sale_price}
-                            discount={prod.discount}
-                            brand={prod.brand}
-                            prodId={prod._id}
-                            free_shipping={prod.free_shipping}
-                            fav={whishlist.includes(prod._id)}
-                            on_sale={prod.on_sale}
-                        />
-                    ))
-                )}
+
+                : <div>
+                    {productsFound === 'loading' ? <Spinner className='cho-svg'/> : <h1>No results</h1>}
+                </div>}
             </div>
 
-            : <div>
-                {productsFound === 'loading' ? <Spinner className='cho-svg'/> : <h1>No results</h1>}
-            </div>}
-
         </div>
+
     </div>
   )
 }
