ed9d3937d656752f169b7289e8a5b5c2d08f5581
Fabricio Repetto <93217713+FabricioJRepetto@users.noreply.github.com>
2022-08-01 19:38:43 -0300

    Update db.js

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const { MONGODB_URI } = process.env;
+const { DB_URL } = process.env;
 
 const clientDb = mongoose.connect(
-  MONGODB_URI,
+  DB_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
