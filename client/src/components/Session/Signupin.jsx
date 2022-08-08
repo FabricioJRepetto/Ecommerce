95b76ef1856abde3e40cf1096936976b7e106948
Fer Ramirez <fer.eze.ram@gmail.com>
2022-08-07 23:20:28 -0300

    sign data removed

--- client/src/components/Session/Signupin.jsx
+++ client/src/components/Session/Signupin.jsx
@@ -172,12 +172,12 @@ const Signupin = () => {
 
   useEffect(() => {
     session && navigate("/");
-    setValueSignin("email", "fer.eze.ram@gmail.com");
+    /* setValueSignin("email", "fer.eze.ram@gmail.com");
     setValueSignin("password", "fer.eze.ram@gmail.com");
     setValueSignup("email", "fer.eze.ram@gmail.com");
     setValueSignup("password", "fer.eze.ram@gmail.com");
     setValueSignup("repPassword", "fer.eze.ram@gmail.com");
-    setValueForgot("email", "fer.eze.ram@gmail.com");
+    setValueForgot("email", "fer.eze.ram@gmail.com"); */
   }, []);
 
   const forgotPassword = async (email) => {
