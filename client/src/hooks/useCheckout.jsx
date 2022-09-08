a96771468c19d389fc54a4105d5d56122084bbf1
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-07 22:10:36 -0300

    notification message changed

--- client/src/hooks/useCheckout.jsx
+++ client/src/hooks/useCheckout.jsx
@@ -1,43 +1,42 @@
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addCart } from "../Redux/reducer/cartSlice";
-import { useNotification } from "./useNotification"
-
+import { useNotification } from "./useNotification";
 
 export const useCheckout = (id) => {
-    const dispatch = useDispatch();
-    const [notification] = useNotification();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [notification] = useNotification();
+  const navigate = useNavigate();
 
-    const { session } = useSelector((state) => state.sessionReducer);
-    const cart = useSelector((state) => state.cartReducer.onCart);
+  const { session } = useSelector((state) => state.sessionReducer);
+  const cart = useSelector((state) => state.cartReducer.onCart);
 
-    const addToCart = async (id) => {
-        if (session) {
-            const { statusText, data } = await axios.post(`/cart/${id}`);
-            statusText === "OK" && !cart.includes(id) && dispatch(addCart(id));
-            notification(
-                data.message,
-                "/cart",
-                `${statusText === "OK" ? "success" : "warning"}`
-            );
-        } else {
-            notification("Necesitas iniciar sesión primero.", "/signin", "warning");
-        }
-    };
+  const addToCart = async (id) => {
+    if (session) {
+      const { statusText, data } = await axios.post(`/cart/${id}`);
+      statusText === "OK" && !cart.includes(id) && dispatch(addCart(id));
+      notification(
+        data.message,
+        "/cart",
+        `${statusText === "OK" ? "success" : "warning"}`
+      );
+    } else {
+      notification("Inicia sesión para comprar", "/signin", "warning");
+    }
+  };
 
-    const buyNow = async (id) => {
-        if (session) {
-            await axios.post(`/cart/`, { product_id: id });
-            navigate("/buyNow");
-        } else {
-            notification("Necesitas iniciar sesión primero.", "/signin", "warning");
-        }
-    };
+  const buyNow = async (id) => {
+    if (session) {
+      await axios.post(`/cart/`, { product_id: id });
+      navigate("/buyNow");
+    } else {
+      notification("Inicia sesión para comprar", "/signin", "warning");
+    }
+  };
 
-    return {
-        addToCart,
-        buyNow
-        }
-}
+  return {
+    addToCart,
+    buyNow,
+  };
+};
