3e48edc22dea4ddae5e43457f9e5b230034c11b7
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-21 19:25:01 -0300

    bugs fixed

--- client/src/components/common/NotFound.jsx
+++ client/src/components/common/NotFound.jsx
@@ -4,20 +4,6 @@ const NotFound = () => {
   return (
     <div className="not-found-container">
       <h1>Sitio no encontrado</h1>
-      <div className="not-found-section"></div>
-      <svg>
-        <filter id="noise">
-          <feTurbulence id="turbulence">
-            <animate
-              attributeName="baseFrequency"
-              dur="50s"
-              values="0.9 0.9;0.8 0.8; 0.9 0.9"
-              repeatCount="indefinite"
-            ></animate>
-          </feTurbulence>
-          <feDisplacementMap in="SourceGraphic" scale="60"></feDisplacementMap>
-        </filter>
-      </svg>
     </div>
   );
 };
