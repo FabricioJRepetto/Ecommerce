8f20d9829d79fa9280003cb84a6621c27c4939af
Fer Ramirez <fer.eze.ram@gmail.com>
2022-09-21 22:45:52 -0300

    404 and 401 buttons added

--- client/src/components/common/NotFound.jsx
+++ client/src/components/common/NotFound.jsx
@@ -1,9 +1,18 @@
 import "./NotFound.css";
+import { useLocation, Link } from "react-router-dom";
+import { ReactComponent as ArrowBack } from "../../assets/svg/arrow-back.svg";
 
 const NotFound = () => {
+  const location = useLocation();
+  const hasPreviousState = location.key !== "default";
+
   return (
     <div className="not-found-container">
       <h1>Sitio no encontrado</h1>
+      <Link to={hasPreviousState ? -2 : "/"}>
+        <ArrowBack />
+        <h2>regresar</h2>
+      </Link>
     </div>
   );
 };
--- client/src/components/common/Unauthorized.jsx
+++ client/src/components/common/Unauthorized.jsx
@@ -1,9 +1,18 @@
 import "./Unauthorized.css";
+import { useLocation, Link } from "react-router-dom";
+import { ReactComponent as ArrowBack } from "../../assets/svg/arrow-back.svg";
 
 const Unauthorized = () => {
+  const location = useLocation();
+  const hasPreviousState = location.key !== "default";
+
   return (
     <div className="unauthorized-container">
       <h1>Sin autorización</h1>
+      <Link to={hasPreviousState ? -1 : "/"}>
+        <ArrowBack />
+        <h2>regresar</h2>
+      </Link>
     </div>
   );
 };
