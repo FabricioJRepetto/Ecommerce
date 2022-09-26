de69894792eb9276111687f06d160222c7a7c2a1
FabricioJRepetto <fabricio.j.repetto@gmail.com>
2022-09-26 15:50:24 -0300

    comment flow working

--- client/src/components/common/Comments/CommentCard.jsx
+++ client/src/components/common/Comments/CommentCard.jsx
@@ -1,31 +1,37 @@
 import React from 'react'
 import Calification from './Calification';
-import { ReactComponent as Edit } from '../../../assets/svg/edit.svg';
 
 const CommentCard = ({props, editable, edit}) => {
-  return (
-    <div className='comment-card-container'>
-        {editable && <div className='comment-card-edit-button'
-            onClick={edit}></div>}
 
-        <div key={props.comment.user_id}>
-            <div className="comment-card-header">
+    const dateGetter = (str) => { 
+        var rx = /^\d{1,2}\/\d{1,2}\/\d{4}/g;
+        var arr = rx.exec(str);
+        return arr[0]; 
+     }
 
-                <div className='comment-user-data'>
-                    <div className='comment-card-image-container'>
-                        <img src={props.user_data.avatar || require('../../../assets/avatardefault.png')} alt="user avatar" />
+    return (
+        <div className='comment-card-container'>
+            {editable && <div className='comment-card-edit-button'
+                onClick={()=>edit(props.comment._id)}></div>}
+
+            <div key={props.comment.user_id}>
+                <div className="comment-card-header">
+
+                    <div className='comment-user-data'>
+                        <div className='comment-card-image-container'>
+                            <img src={props.user_data.avatar || require('../../../assets/avatardefault.png')} alt="user avatar" />
+                        </div>
+                        <h2>{props.user_data.name}</h2>
                     </div>
-                    <h2>{props.user_data.name}</h2>
+                    <Calification num={props.comment.calification}/>                
                 </div>
-                <Calification num={props.comment.calification}/>                
+
+                <div className="comment-card-text">{props.comment.text}</div>
+
+                <div className="comment-card-date">{dateGetter(props.comment.date)}</div>
             </div>
-
-            <div className="comment-card-text">{props.comment.text}</div>
-
-            <div className="comment-card-date">{props.comment.date.split(' ')[0]}</div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default CommentCard
